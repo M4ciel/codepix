b2f788be28356525ab34262f8761ee82afff12ab
Caio Maciel <caiolmaciell@gmail.com>
2023-10-23 19:45:35 -0300

    :tada: Commit inicial

---
+++ domain/model/account.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+type Account struct {
+	Base      `valid:"required"`
+	OwnerName string    `json:"owner_name" valid:"notnull"`
+	Bank      *Bank     `valid:"-"`
+	Number    string    `json:"number" valid:"notnull"`
+	PixKeys   []*PixKey `valid:"-"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewAccount(bank *Bank, number string, ownerName string) (*Account, error) {
+	account := Account{
+		OwnerName: ownerName,
+		Bank:      bank,
+		Number:    number,
+	}
+
+	account.ID = uuid.NewV4().String()
+	account.CreatedAt = time.Now()
+
+	error := account.isValid()
+	if error != nil {
+		return nil, error
+	}
+
+	return &account, nil
+}
